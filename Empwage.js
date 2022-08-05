94f418ccb76f5834fda73ba9f14b0f67d1c00b98
VinayDanagoud <vinaydanagoud143@gmail.com>
2022-08-05 16:38:15 +0530

    Refactor Code With Fuction

--- Empwage.js
+++ Empwage.js
@@ -1,34 +1,25 @@
-// UC1 IF ELSE condition
-{
-   const Is_ABSENT = 0
-   let empCheck = Math.floor(Math.random() * 10) % 2;
-   if (empCheck == Is_ABSENT) {
-      console.log("UC -1 Employee is Absent.exiting the program");
-      return;
-    } else {
-       console.log("UC1 - Employee is PRESENT");
-    }
-}
 
-// UC2 Switch Case
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
+
+
+function getWorkingHours(empCheck) {
 switch (empCheck) {
     case IS_PART_TIME:
-            empHrs = PART_TIME_HOURS;
-            break;
-    case IS_FULL_TIME:
-            empHrs = FULL_TIME_HOURS;
-            break;
+           return PART_TIME_HOURS;
+     case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
     default:
-           empHrs = 0;
+           return 0;
+    }
 }
 
+let empCheck = Math.floor(Math.random() * 10) % 3;
+let empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("UC2 - Emp Wage: " + empWage);
+console.log("Emp Wage: " + empWage);
+
